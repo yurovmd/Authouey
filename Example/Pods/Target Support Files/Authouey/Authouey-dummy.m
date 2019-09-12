d1c73127be3b3f00367168b8e954f730d359a116
Maxim Yurov <v.maksim.yurov@reddit.com>
2019-09-12 12:49:23 +0300

    Submodule commit

---
+++ Example/Pods/Target Support Files/Authouey/Authouey-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Authouey : NSObject
+@end
+@implementation PodsDummy_Authouey
+@end
---
+++ Example/Pods/Target Support Files/Pods-Authouey_Tests/Pods-Authouey_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Authouey_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Authouey_Tests
+@end
